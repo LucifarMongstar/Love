6e31a2eb14fd19cefcd96877809090f835e1796e
ＬＵＣＩＦＥＲ ᴹᵒⁿᵍˢᵗᵃʳ <59021986+LucifarMongstar@users.noreply.github.com>
2021-02-14 13:20:57 +0200

    Add files via upload

---
+++ love.py
@@ -0,0 +1,65 @@
+import os
+from os import *
+import sys
+import time
+system("pkg install figlet")
+os.system("clear")
+######################
+text = '''
+          ......       ......
+        .:oOOOOo:.   .:oOOOOo:.
+      .:oOO:'':Oo:. .:oO:'':OOo:.
+     .:oO:      'Oo:oO'      :Oo:.
+     :oO:         'o'         :Oo:
+     :oO:                     :Oo:
+     ':oO:                   :Oo:'
+      ':oO:       B E       :Oo:'
+        ':oO.             .Oo:'
+          ':oO.         .Oo:'
+            ':oO.     .Oo:'
+              ':oO. .Oo:'
+                'oO:Oo'
+          ...... 'oOo' ......
+                  'o'
+'''
+print (text)
+time.sleep(0.5)
+#######################
+love = input ("What is your sweetheart's name : ")
+text = '''\033[1;91m
+               .-"""-.    .-"""-.
+              /       `..'       \
+             |                    |
+             |      H A P P Y     |
+              \    VALENTINE'S   /
+             __\     D A Y !    /
+        _   /  |`\            /'
+       | \  \/_/  `\        /'
+       \_\| / __    `\    /'
+          \/_/__\     `\/'  .--='/~\
+   ____,__/__,_____,______)/   /{~}}}
+   -,-----,--\--,-----,---,\'-' {{~}}
+           __/\_            '--=.\}/
+          /_/ |\\
+               \/
+
+'''
+print (text)
+time.sleep(0.5)
+print ("\033[0;32m")
+system("figlet " +love)
+time.sleep(0.5)
+text = '''
+                       _  _
+                      ( \/ )
+               .---.   \  /   .-"-.
+              /   6_6   \/   / 4 4 \
+                                           \_  (__\       \_ v _/
+              //   \\        //   \\
+             ((     ))      ((     ))
+       =======""===""========""===""=======
+                |||            |||
+                 |              |
+
+'''
+print (text)
